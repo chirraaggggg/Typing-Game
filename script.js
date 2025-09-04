f1a6efd81a99772dc500432369db559e26f34252
Chirag <chhirag920@gmail.com>
2025-09-05 00:49:47 +0530

    added javascript

---
+++ script.js
@@ -0,0 +1,24 @@
+const RANDOM_QUOTE_API_URL = 'https://api.quotable.io/random?tags=&author='
+const FALLBACK_QUOTE_API_URL = 'https://dummyjson.com/quotes/random'
+const quoteDisplayElement = document.getElementById('quoteDisplay')
+const quoteInputElement = document.getElementById('quoteInput')
+const timerElement = document.getElementById('timer')
+
+let correct = true
+let startTime
+let timerInterval
+
+function startTimer() {
+  timerElement.innerText = 0
+  startTime = new Date()
+  timerInterval = setInterval(() => {
+    timerElement.innerText = getTimerTime()
+  }, 1000)
+}
+
+function stopTimer() {
+  if (timerInterval) {
+    clearInterval(timerInterval)
+    timerInterval = null
+  }
+}
